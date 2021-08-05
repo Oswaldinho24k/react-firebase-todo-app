64733a241e0f7a8eaacfef357305bdbb92cc2317
Oswaldo Martinez <oswalfut_96@hotmail.com>
2021-08-05 08:21:46 -0500

    add envs

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -5,12 +5,12 @@ import "firebase/firestore";
 // Initialize Firebase
 // use ENVS
 const firebaseConfig = {
-  apiKey: process.env.REACT_API_KEY,
-  authDomain: process.env.REACT_AUTH_DOMAIN,
-  projectId: process.env.REACT_PROJECT_ID,
-  storageBucket: process.env.REACT_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_ID,
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
 };
 firebase.initializeApp(firebaseConfig);
 
