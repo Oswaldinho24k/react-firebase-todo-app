ef2576cc9972dd17cee833152dec4f969bea8896
Oswaldo Martinez <oswalfut_96@hotmail.com>
2021-08-05 01:59:53 -0500

    envs

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -5,12 +5,12 @@ import "firebase/firestore";
 // Initialize Firebase
 // use ENVS
 const firebaseConfig = {
-  apiKey: "AIzaSyBfDSYHl2GxahwUKvEBdlsyF19fcycptsg",
-  authDomain: "oswaldinho-demos-657b9.firebaseapp.com",
-  projectId: "oswaldinho-demos-657b9",
-  storageBucket: "oswaldinho-demos-657b9.appspot.com",
-  messagingSenderId: "135802538376",
-  appId: "1:135802538376:web:ea43c30b2fccf98ff4d23d",
+  apiKey: process.env.REACT_API_KEY,
+  authDomain: process.env.REACT_AUTH_DOMAIN,
+  projectId: process.env.REACT_PROJECT_ID,
+  storageBucket: process.env.REACT_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_ID,
 };
 firebase.initializeApp(firebaseConfig);
 
